8184174fec3510f32abdc4b3047f543c2df4250a
xX800R7b <alxsimpson@gmail.com>
2020-12-10 15:51:04 +1000

    added func hovering circle

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4 {
         this.cols = 7;
         this.selector = selector;
         this.createGrid();
+        this.setupEventListeners();
     }
 
     createGrid() {
@@ -14,10 +15,38 @@ class Connect4 {
                 .addClass('row');
             for (let col = 0; col < this.cols; col++) {
                 const $col = $('<div>')
-                    .addClass('col empty');
+                    .addClass('col empty')
+                    .attr('data-col', col)
+                    .attr('data-row', row);
                 $row.append($col);
             }
             $board.append($row);
         }
+    }
+
+    setupEventListeners() {
+        const $board = $(this.selector);
+
+        function findLastEmptyCell(col) {
+            const cells = $(`.col[data-col='${col}']`);
+            for (let i = cells.length -1; i >= 0; i--) {
+                const $cell = $(cells[i]);
+                if($cell.hasClass('empty')) {
+                    return $cell;
+                }
+            }
+            return null;
+            console.log(cells);
+        }
+
+        $board.on('mouseenter', '.col.empty', function() {
+            const col = $(this).data('col');
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.addClass('next-red');
+        })
+
+        $board.on('mouseleave', '.col.empty', function() {
+            $('.col').removeClass('next-red');
+        })
     }
 }
