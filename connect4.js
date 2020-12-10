52839ad322295385b035e8d0c5b0e05c8331b86b
xX800R7b <alxsimpson@gmail.com>
2020-12-10 16:24:00 +1000

    two player functionality

--- connect4.js
+++ connect4.js
@@ -2,6 +2,7 @@ class Connect4 {
     constructor(selector) {
         this.rows = 6;
         this.cols = 7;
+        this.player = 'red';
         this.selector = selector;
         this.createGrid();
         this.setupEventListeners();
@@ -26,6 +27,7 @@ class Connect4 {
 
     setupEventListeners() {
         const $board = $(this.selector);
+        const that = this;
 
         function findLastEmptyCell(col) {
             const cells = $(`.col[data-col='${col}']`);
@@ -42,11 +44,20 @@ class Connect4 {
         $board.on('mouseenter', '.col.empty', function() {
             const col = $(this).data('col');
             const $lastEmptyCell = findLastEmptyCell(col);
-            $lastEmptyCell.addClass('next-red');
+            $lastEmptyCell.addClass(`next-${that.player}`);
         })
 
         $board.on('mouseleave', '.col.empty', function() {
-            $('.col').removeClass('next-red');
-        })
+            $('.col').removeClass(`next-${that.player}`);
+        });
+
+        $board.on('click', '.col.empty', function() {
+            const col = $(this).data('col');
+            const row = $(this).data('row');
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.removeClass('empty');
+            $lastEmptyCell.addClass(that.player);
+            that.player = (that.player === 'red') ? 'blue' : 'red';
+        });
     }
 }
